44584b4b5a8ff38a5f402557d9bf4e8c87a204eb
Naiara Abaroa <naiaraabaroa@gmail.com>
2019-11-16 13:22:43 +0100

    Distribute components inidividually

--- TODO.md
+++ TODO.md
@@ -4,5 +4,4 @@
 - Add correct badges. npm version and license
 - Simpliply all the components
 - Find a font to use easily with variable fonts
-- Also distribute the components individually
-- .section for vertical spacing of the sections
+- Add vue for nak-docs
--- gulpfile.js
+++ gulpfile.js
@@ -1,60 +1,72 @@
-const gulp = require('gulp');
-const postcss = require('gulp-postcss');
-const autoprefixer = require('autoprefixer');
-const postcssPresetEnv = require('postcss-preset-env');
-const cssImport = require('postcss-import');
-const customProperties = require('postcss-custom-properties');
-const apply = require('postcss-apply');
-const mixins = require('postcss-mixins');
-const nested = require('postcss-nested');
-const customMedia = require('postcss-custom-media');
-const cssnano = require('cssnano');
-const notify = require('gulp-notify');
-const rename = require('gulp-rename');
-const inject = require('gulp-inject-string');
-const replace = require('gulp-string-replace');
-const insert = require('gulp-insert');
+const gulp = require("gulp");
+const postcss = require("gulp-postcss");
+const autoprefixer = require("autoprefixer");
+const postcssPresetEnv = require("postcss-preset-env");
+const cssImport = require("postcss-import");
+const customProperties = require("postcss-custom-properties");
+const apply = require("postcss-apply");
+const mixins = require("postcss-mixins");
+const nested = require("postcss-nested");
+const customMedia = require("postcss-custom-media");
+const cssnano = require("cssnano");
+const notify = require("gulp-notify");
+const rename = require("gulp-rename");
+const inject = require("gulp-inject-string");
+const replace = require("gulp-string-replace");
+const insert = require("gulp-insert");
 
 function css() {
   return gulp
     .src("./src/nakDS.css")
-    .pipe(postcss([cssImport(),mixins(),postcssPresetEnv(),autoprefixer()]))
-    .pipe(gulp.dest("./dist/css/"))
+    .pipe(postcss([cssImport(), mixins(), postcssPresetEnv(), autoprefixer()]))
     .pipe(postcss([cssnano()]))
-    .pipe(rename({ suffix: ".min" }))
     .pipe(gulp.dest("./dist/css/"))
-    .pipe(notify({
-      message: 'Your nakDS CSS is ready ♡'
-    }));
-  }
+    .pipe(
+      notify({
+        message: "Your nakDS CSS is ready ♡"
+      })
+    );
+}
 
-  function utils() {
-    return gulp
-      .src("./src/variables/core-utils.css")
-      .pipe(postcss([cssImport(),mixins(),postcssPresetEnv(),autoprefixer()]))
-      .pipe(gulp.dest("./dist/css/"))
-      .pipe(postcss([cssnano()]))
-      .pipe(rename({ suffix: ".min" }))
-      .pipe(gulp.dest("./dist/css/"))
-      .pipe(notify({
-        message: 'Your utils are ready ♡'
-      }));
-    }
+function utils() {
+  return gulp
+    .src("./src/variables/core-utils.css")
+    .pipe(postcss([cssImport(), mixins(), postcssPresetEnv(), autoprefixer()]))
+    .pipe(postcss([cssnano()]))
+    .pipe(gulp.dest("./dist/css/"))
+    .pipe(
+      notify({
+        message: "Your utils are ready ♡"
+      })
+    );
+}
 
-  function fonts() {
-    return gulp
-    .src("./assets/fonts/*.*")
-    .pipe(gulp.dest("./dist/assets/fonts/"))
-  }
+function components() {
+  return gulp
+    .src("./src/components/**.css")
+    .pipe(postcss([cssImport(), mixins(), postcssPresetEnv(), autoprefixer()]))
+    .pipe(postcss([cssnano()]))
+    .pipe(gulp.dest("./dist/css/components/"))
+    .pipe(
+      notify({
+        message: "Your components are ready ♡"
+      })
+    );
+}
+
+function fonts() {
+  return gulp.src("./assets/fonts/*.*").pipe(gulp.dest("./dist/assets/fonts/"));
+}
 
 function watch() {
   gulp.watch("./src/**/*.css", css);
 }
 
-const build = gulp.series(css, utils, fonts, watch);
+const build = gulp.series(css, utils, components, fonts, watch);
 
 exports.css = css;
 exports.utils = utils;
+exports.components = components;
 exports.fonts = fonts;
 exports.watch = watch;
 exports.default = build;
