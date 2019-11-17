6e76b21325e34dd7ce3f237fa9baa6d31b517277
Naiara Abaroa <naiaraabaroa@gmail.com>
2019-11-17 15:24:14 +0100

    Add trial to distribute tokens

--- gulpfile.js
+++ gulpfile.js
@@ -18,7 +18,15 @@ const insert = require("gulp-insert");
 function css() {
   return gulp
     .src("./src/nakDS.css")
-    .pipe(postcss([cssImport(), mixins(), nested(), postcssPresetEnv(), autoprefixer()]))
+    .pipe(
+      postcss([
+        cssImport(),
+        mixins(),
+        nested(),
+        postcssPresetEnv(),
+        autoprefixer()
+      ])
+    )
     .pipe(postcss([cssnano()]))
     .pipe(gulp.dest("./dist/css/"))
     .pipe(gulp.dest("./docs/css/"))
@@ -32,7 +40,15 @@ function css() {
 function utils() {
   return gulp
     .src("./src/variables/core-utils.css")
-    .pipe(postcss([cssImport(), mixins(), nested(), postcssPresetEnv(), autoprefixer()]))
+    .pipe(
+      postcss([
+        cssImport(),
+        mixins(),
+        nested(),
+        postcssPresetEnv(),
+        autoprefixer()
+      ])
+    )
     .pipe(postcss([cssnano()]))
     .pipe(gulp.dest("./dist/css/"))
     .pipe(
@@ -45,7 +61,15 @@ function utils() {
 function components() {
   return gulp
     .src("./src/components/**.css")
-    .pipe(postcss([cssImport(), mixins(), nested(), postcssPresetEnv(), autoprefixer()]))
+    .pipe(
+      postcss([
+        cssImport(),
+        mixins(),
+        nested(),
+        postcssPresetEnv(),
+        autoprefixer()
+      ])
+    )
     .pipe(postcss([cssnano()]))
     .pipe(gulp.dest("./dist/css/components/"))
     .pipe(
@@ -56,27 +80,58 @@ function components() {
 }
 
 function fonts() {
-  return gulp.src("./assets/fonts/*.*")
-  .pipe(gulp.dest("./dist/assets/fonts/"))
-  .pipe(gulp.dest("./docs/assets/fonts/"));
+  return gulp
+    .src("./assets/fonts/*.*")
+    .pipe(gulp.dest("./dist/assets/fonts/"))
+    .pipe(gulp.dest("./docs/assets/fonts/"));
 }
 
 function svg() {
-  return gulp.src("./assets/svg/*.*")
-  .pipe(gulp.dest("./dist/assets/svg/"))
-  .pipe(gulp.dest("./docs/assets/svg/"));
+  return gulp
+    .src("./assets/svg/*.*")
+    .pipe(gulp.dest("./dist/assets/svg/"))
+    .pipe(gulp.dest("./docs/assets/svg/"));
 }
 
 function watch() {
   gulp.watch("./src/**/*.css", css);
 }
 
-const build = gulp.series(css, utils, components, fonts, svg, watch);
+function colorTokens() {
+  return gulp
+    .src("./src/tokens/tokens--color.css")
+    .pipe(postcss([cssnano()]))
+    .pipe(
+      inject.beforeEach("--color", '<span class="nk-box" style="background:')
+    )
+    .pipe(replace(/--\S+\:/g, ""))
+    .pipe(inject.afterEach(";", '"></span>'))
+    .pipe(replace(":root{", '<div class="nk-section">'))
+    .pipe(replace("}", "</div>"))
+    .pipe(rename("tokens--color.html"))
+    .pipe(gulp.dest("./docs/"))
+    .pipe(
+      notify({
+        message: "Your COLOR TOKEN HTML is ready ♡ "
+      })
+    );
+}
+
+const build = gulp.series(
+  css,
+  utils,
+  components,
+  colorTokens,
+  fonts,
+  svg,
+  watch
+);
 
 exports.css = css;
 exports.utils = utils;
 exports.components = components;
 exports.fonts = fonts;
 exports.svg = svg;
+exports.colorTokens = colorTokens;
 exports.watch = watch;
 exports.default = build;
